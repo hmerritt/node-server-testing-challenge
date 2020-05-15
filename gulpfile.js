646537ff27644cce924fa3143353684f6e172f2a
Harry Merritt <harry@merritt.es>
2020-05-15 01:01:55 +0100

    Update gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const prettier = require("gulp-prettier");
 
 gulp.task("default", () => {
     return gulp
-        .src(["enhancing/**/*.js", "package.json", "gulpfile.js"])
+        .src(["api/**/*.js", "data/**/*.js", "package.json", "gulpfile.js"])
         .pipe(prettier({ editorconfig: true }))
         .pipe(gulp.dest((file) => file.base));
 });
