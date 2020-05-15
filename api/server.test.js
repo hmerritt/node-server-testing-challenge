8e7c68d1ce63a5520eb265ddd6e2b990300ba7c8
Harry Merritt <harry@merritt.es>
2020-05-15 03:16:57 +0100

    Test Delete /item/:id endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -44,7 +44,8 @@ describe("server", () => {
 
         it("returns error message for invalid item", async () => {
             const res = await request(server).get("/items/404");
-            expect(res.body.message).toEqual("Somthing went wrong");
+            expect(res.status).toBe(400);
+            expect(res.body.message).toEqual("key does not exist in db: 404");
         });
     });
 
@@ -52,20 +53,50 @@ describe("server", () => {
         it("returns HTTP 400 on malformed request", async () => {
             const res = await request(server).post("/items");
             expect(res.status).toBe(400);
-            expect(res.body.message).toEqual("missing post data { name: 'your item here' }");
+            expect(res.body.message).toEqual(
+                "missing post data { name: 'your item here' }"
+            );
         });
 
         it("returns new item added to db", async () => {
-            const res = await request(server).post("/items").send({name: 'new-item'});
+            const res = await request(server)
+                .post("/items")
+                .send({ name: "new-item" });
             expect(res.status).toBe(200);
             expect(res.body.id).toBe(4);
             expect(res.body.name).toEqual("new-item");
         });
 
         it("adds new item to db", async () => {
-            const res = await request(server).post("/items").send({name: 'new-item'});
+            const res = await request(server)
+                .post("/items")
+                .send({ name: "new-item" });
             const resAll = await request(server).get("/items");
-            expect(resAll.body[4]).toEqual({id: 5, name: 'new-item'});
+            expect(resAll.body[4]).toEqual({ id: 5, name: "new-item" });
+        });
+    });
+
+    describe("DELETE /items/:id", () => {
+        it("returns HTTP 400 on malformed request", async () => {
+            const res = await request(server).delete("/items/404");
+            expect(res.status).toBe(400);
+            expect(res.body.message).toEqual("key does not exist in db: 404");
+        });
+
+        it("returns deleted item from db", async () => {
+            const res = await request(server).delete("/items/1");
+            expect(res.status).toBe(200);
+            expect(res.body).toEqual({
+                id: 1,
+                name: "pen",
+            });
+        });
+
+        it("deletes item from db", async () => {
+            const res = await request(server).delete("/items/2");
+            const resAll = await request(server).get("/items");
+            const item = resAll.body.find((item) => item.id === 2);
+            expect(item).toBeUndefined();
         });
     });
 });
